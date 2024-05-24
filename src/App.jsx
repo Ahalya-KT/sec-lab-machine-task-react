15020820e28c70a443ef4409f15a11c7105bd919
Ahalya-KT <ahalyakt002@gmail.com>
2024-05-24 17:18:15 +0530

    machine task project created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-4xl">Hello World</div>
+    </>
+  );
+}
+
+export default App;
